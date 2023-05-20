119afec944c8062e33f1018327f3ee1e38406eda
0-ayano <ac234704@edu.okinawa-ct.ac.jp>
2023-05-20 12:55:50 +0900

    APIのテスト済み

---
+++ src/views/HomeView.vue
@@ -0,0 +1,42 @@
+<template>
+    <iframe 
+        v-for="(data, index) in URL"
+        class = schedule
+        v-bind:src = URL[index]
+    ></iframe>
+</template>
+
+<script setup>
+import Event from '../assets/event.json'
+import { ref, onMounted } from 'vue'
+
+const eventData = Event.event
+const URL = ref([])
+
+const createURL = (title, description, date_start, date_end) => {
+    return "http://127.0.0.1:8000/api?title=" + title + "&description=" + description + "&dates=" + date_start + "/" + date_end
+}
+
+onMounted(() => {
+    for(var i=0; i<Object.keys(Event.event).length; i++){
+        const title       = eventData[i]["title"]
+        const description = eventData[i]["description"]
+        const date_start  = eventData[i]["date_start"]
+        const date_end    = eventData[i]["date_end"]
+
+        URL.value.push( createURL(title, description, date_start, date_end) )
+    }
+})
+
+</script>
+
+<style scoped>
+.schedule{
+    border: 0;
+    position: relative;
+    width: 60%;
+    left: 50%;
+    transform: translateX(-50%);
+}
+
+</style>
